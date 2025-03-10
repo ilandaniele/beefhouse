51693e2773ad48f0280ba694499e3191514295d0
Ilan Daniele <ilan.daniele@gmail.com>
2025-03-09 21:49:33 -0300

    testing pata

--- pages/producto/[id].tsx
+++ pages/producto/[id].tsx
@@ -9,7 +9,7 @@ const productos: Producto[] = [
   {
     id: 0,
     nombre: 'Patas de Pollo',
-    imagen: '.next/images/chicken/pata.png',
+    imagen: '/images/chicken/pata.png',
     precioPorKilo: 8.99,
     category: 'pollo',
   },
